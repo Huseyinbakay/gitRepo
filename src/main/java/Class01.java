89f3afe01dc75ced3dc36cf9e3b1ff26d83088c7
Huseyinbakay <avci_4506@hotmail.com>
2023-08-19 20:26:15 +0300

    Version 1

---
+++ src/main/java/Class01.java
@@ -0,0 +1,31 @@
+public class Class01 {
+
+    public static void main(String[] args) {
+
+
+
+
+        int x = 1;
+        int y =2;
+
+        System.out.println(x+y);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
