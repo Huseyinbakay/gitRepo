62c5a19205cd3765fb625775069537428b5713d3
Huseyinbakay <avci_4506@hotmail.com>
2023-08-19 21:43:37 +0300

    Version

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -10,12 +10,27 @@ public class Class01 {
 
         System.out.println(x+y);
 
-
+//git init ile terminalden kaydediyoruz.
+//git status statüyü kontrol ettiriyor
+//git add . ile tüm eksikleri staging area'ya yükler. Geçici alana yani.
+//git commit -m" başlık gelecek buraya "
+//git log ile versiyon görünür bilgiler
+//cls ile alan temizleniyor kodlar yani
+//git commit --amend -m""  isim değiştirme (hangi versiyondaysan)
+        // klavyeedeki ok tuşlarıyla eski yazdıklarına ulaşabilirsin
+//git log --oneline ile versiyonları tek satırlar hali
 
         int  z =5;
         System.out.println(x+y+z);
 
         System.out.println("Versiyon 3 için kodlar");
+
+
+        //versiyon 4 kodları
+
+
+        System.out.println("hatalı kodlar........");
+
 
 
 
